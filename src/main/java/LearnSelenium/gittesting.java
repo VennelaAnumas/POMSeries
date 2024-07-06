fc82d45a84cb2956644d646f06f92d5e68f94d79
Vennela Anumas <vennelareddyanumas@gmail.com>
2024-07-06 18:14:45 +0530

    commiting gittesting class

---
+++ src/main/java/LearnSelenium/gittesting.java
@@ -0,0 +1,16 @@
+package LearnSelenium;
+
+public class gittesting {
+
+	public static void doclick()
+	{
+		System.out.println("doclick");
+	}
+	
+	public static void main(String[] args) {
+int i=10;
+doclick();
+
+	}
+
+}
