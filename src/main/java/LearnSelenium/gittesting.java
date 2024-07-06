5f5c7a87f02a0fdca94c8e375e1b4bf69d2487bf
Vennela Anumas <vennelareddyanumas@gmail.com>
2024-07-06 20:00:30 +0530

    addressed PR review comments for gittesting

--- src/main/java/LearnSelenium/gittesting.java
+++ src/main/java/LearnSelenium/gittesting.java
@@ -4,11 +4,15 @@ public class gittesting {
 
 	public static void doclick()
 	{
+		
 		System.out.println("doclick");
+		
 	}
 	
 	public static void main(String[] args) {
 int i=10;
+System.out.println(i);
+
 doclick();
 
 	}
