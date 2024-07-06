77ff927f5fa9681973cc6609ea631751d4f0209c
Vennela Anumas <vennelareddyanumas@gmail.com>
2024-07-06 20:39:57 +0530

    commiting car class

---
+++ src/main/java/LearnSelenium/car.java
@@ -0,0 +1,14 @@
+package LearnSelenium;
+
+public class car {
+
+	
+	public static void printModel()
+	{
+		System.out.println("baleano");
+	}
+	public static void main(String[] args) {
+		printModel();
+	}
+
+}
